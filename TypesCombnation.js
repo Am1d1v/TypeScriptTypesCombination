37c7d91b98333f20bffcb6574a92a96b241d72e9
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-28 13:39:26 +0300

    Add files via upload

--- TypesCombnation.js
+++ TypesCombnation.js
@@ -1,10 +1,43 @@
 // Conditional Type
-var x = 10;
-var isNegative = x >= 0 ? "no" : "yes";
-var obj1 = {
+/*
+interface StringRecord {
+    [key: string]: string;
+}
+
+interface DateRecord {
+    [key: string]: Date;
+}
+
+type MyRecord<T> = T extends string ? StringRecord : DateRecord;
+type Object1 = MyRecord<string>;
+type Object2 = MyRecord<number>;
+
+const obj1: Object1 = {
     a: '123',
     b: '234'
-};
-var obj2 = {
+}
+
+const obj2: Object2 = {
     a: new Date(1990, 5, 5)
-};
+}
+*/
+// Extends
+/*
+type answer1 = 60 extends number ? true : false; // true
+type answer2 = number extends 64 ? true : false; // false
+type answer3 = string[] extends any ? true : false; // true
+type answer4 = string[] extends any[] ? true : false; // true
+type answer5 = never extends any ? true : false; // true
+type answer6 = any extends any ? true : false; // true
+*/
+// Infer
+function fromPair(pair) {
+    var _a;
+    var key = pair[0], value = pair[1];
+    return _a = {},
+        _a[key] = value,
+        _a;
+}
+var myPair1 = ['myKey1', 'myValue1'];
+var pairRes = fromPair(myPair1);
+console.log(pairRes);
--- TypesCombnation.ts
+++ TypesCombnation.ts
@@ -26,17 +26,28 @@ const obj2: Object2 = {
 
 // Extends
 
+/*
 type answer1 = 60 extends number ? true : false; // true
 type answer2 = number extends 64 ? true : false; // false
 type answer3 = string[] extends any ? true : false; // true
 type answer4 = string[] extends any[] ? true : false; // true
 type answer5 = never extends any ? true : false; // true
 type answer6 = any extends any ? true : false; // true
+*/
 
+// Infer
 
+function fromPair(pair: [string, string]){
+    const [key, value] = pair;
 
+    return {
+        [key]: value
+    }
+}
 
-
+const myPair1 = ['myKey1', 'myValue1'];
+const pairRes =  fromPair(myPair1 as [string, string]);
+console.log(pairRes);
 
 
 
